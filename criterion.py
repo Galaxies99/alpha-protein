9aceb57c77b0b69fc90f38b0895791b5f336b686
Tony Fang <galaxies@vip.qq.com>
2021-04-21 16:38:22 +0800

    update initial: criterion & dataset.

---
+++ criterion.py
@@ -0,0 +1,62 @@
+import numpy as np
+from sklearn.metrics import precision_score
+
+
+def cal_acc(label, mask, large_20, pred):
+    result = np.zeros([4])
+
+    mask_tmp = np.triu(mask, 0)
+
+    L = label.shape[0]
+    effect = np.where(mask_tmp.reshape(-1) != 0)
+    label = label.reshape(-1)
+    label = label[effect]
+    pred = pred.reshape(-1)
+    pred = pred[effect]
+    large_20 = large_20.reshape(-1)
+    large_20 = large_20[effect]
+    order = np.argsort(large_20)[::-1]
+
+    topL_10 = int(np.ceil(L/10))
+    result[0] = precision_score(label[order[:topL_10]], pred[order[:topL_10]], average='weighted')
+    topL_5 = int(np.ceil(L/5))
+    result[1] = precision_score(label[order[:topL_5]], pred[order[:topL_5]], average='weighted')
+    topL_2 = int(np.ceil(L/2))
+    result[2] = precision_score(label[order[:topL_2]], pred[order[:topL_2]], average='weighted')
+    topL = int(np.ceil(L))
+    result[3] = precision_score(label[order[:topL]], pred[order[:topL]], average='weighted')
+
+    return result
+
+
+def cal_top(label, mask, pred):  
+    acc = np.zeros([2, 4])
+    large_20 = pred[9, :, :]
+    large_20 = 1 - large_20
+    pred_final = np.argmax(pred, 0)
+    trunc_mat = np.zeros(label.shape)
+    nn = label.shape[0]
+    for kk in range(7):
+        if kk != 0:
+            trunc_mat = trunc_mat + np.diag(np.ones(nn - kk), kk) + np.diag(np.ones(nn - kk), -kk)
+        else:
+            trunc_mat = trunc_mat + np.diag(np.ones(nn - kk), kk)
+
+    trunc_mat_tmp = np.ones(trunc_mat.shape) - trunc_mat
+    mask = mask * trunc_mat_tmp
+    acc[0, :] = cal_acc(label, mask, large_20, pred_final)
+
+    trunc_mat = np.zeros(label.shape)
+    nn = label.shape[0]
+    for kk in range(25):
+        if kk != 0:
+            trunc_mat = trunc_mat + np.diag(np.ones(nn - kk), kk) + np.diag(np.ones(nn - kk), -kk)
+        else:
+            trunc_mat = trunc_mat + np.diag(np.ones(nn - kk), kk)
+
+    trunc_mat_tmp = np.ones(trunc_mat.shape) - trunc_mat
+    mask = mask * trunc_mat_tmp
+    acc[1, :] = cal_acc(label, mask, large_20, pred_final)
+    return acc
+
+
---
+++ dataset.py
@@ -0,0 +1,52 @@
+import os
+import os.path as osp
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import random
+import tarfile
+import shutil
+
+
+
+class Protein_data(Dataset):
+    def __init__(self, root, train=False):
+        self.dist_dir = 'your_label_dir'
+        self.feature_dir = 'your_feature_dir'
+        self.proteins = os.listdir(self.dist_dir)
+
+    def __getitem__(self, index):
+        prot_name = self.proteins[index]
+        prot_name = prot_name[:prot_name.find('.')]
+        dist = self.get_label(self.dist_dir+'/'+prot_name+'.npy')
+        feature = self.get_feature(self.feature_dir+'/'+prot_name + '.npy.gz')
+        mask = np.where(dist==-1, 0, 1)
+        label = np.zeros(dist.shape)
+        label += np.where((dist >= 4) & (dist < 6), np.ones_like(label), np.zeros_like(label))
+        label += np.where((dist >= 6) & (dist < 8), np.ones_like(label)*2, np.zeros_like(label))
+        label += np.where((dist >= 8) & (dist < 10), np.ones_like(label)*3, np.zeros_like(label))
+        label += np.where((dist >= 10) & (dist < 12), np.ones_like(label)*4, np.zeros_like(label))
+        label += np.where((dist >= 12) & (dist < 14), np.ones_like(label)*5, np.zeros_like(label))
+        label += np.where((dist >= 14) & (dist < 16), np.ones_like(label)*6, np.zeros_like(label))
+        label += np.where((dist >= 16) & (dist < 18), np.ones_like(label)*7, np.zeros_like(label))
+        label += np.where((dist >= 18) & (dist < 20), np.ones_like(label)*8, np.zeros_like(label))
+        label += np.where((dist >= 20), np.ones_like(label)*9, np.zeros_like(label))
+        return feature, label, mask
+
+    def __len__(self):
+        return len(self.proteins)
+
+    def get_label(self, name):
+        tmp_label = np.load(name)
+        return tmp_label
+
+    @staticmethod
+    def get_feature(name):
+        f_name = name.replace(".npy.gz","")
+        g_file = tarfile.open(name)
+        g_file.extractall(f_name)
+        dir_ = os.listdir(f_name)
+        tmp_feature = np.load(f_name+'/'+dir_[0])
+        tmp_feature = np.transpose(tmp_feature, (2,0,1))
+        shutil.rmtree(f_name)
+        return tmp_feature
