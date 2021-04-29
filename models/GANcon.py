24858a7d23f5483d84a730dc475f8c4df45c30c2
Tony Fang <galaxies@vip.qq.com>
2021-04-29 17:59:37 +0800

    Delete GANcon

--- models/GANcon.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-import torch.nn.functional as F
-
-class GANcon(nn.Module):
-    def __init__(self, args = {}):
-        self(GANcon, self).__init__()
-
-    # TODO: Complete the GANcon model by 2021/5/5, task assigned to Hongjie Fang.
-    
