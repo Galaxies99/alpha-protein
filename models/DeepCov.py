Fix bugs of activation, add softmax option in configs.

models/DeepCov.py
@@ -25,11 +25,7 @@ class DeepCov(nn.Module):
             )
         self.interConvList = nn.ModuleList(self.interConvList)
 
-        self.OutConv = nn.Sequential(
-            nn.Conv2d(64, out_channel, kernel_size=(1, 1)),
-            nn.BatchNorm2d(10),
-            nn.Sigmoid()
-        )
+        self.OutConv = nn.Conv2d(64, out_channel, kernel_size=(1, 1))
 
     def forward(self, x):
         x = self.MaxOutConv(x)

models/SampleNet.py
@@ -33,10 +33,7 @@ class SampleNet(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU()
         )
-        self.final = nn.Sequential(
-            nn.Conv2d(64, out_channel, 1),
-            nn.Sigmoid()
-        )
+        self.final = nn.Conv2d(64, out_channel, 1)
 
     def forward(self, x):
         y1 = self.conv1(x)