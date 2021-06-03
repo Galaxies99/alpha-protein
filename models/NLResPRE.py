a55597b6158d20fa37f4807849acd14a20855e76
Tony Fang <galaxies@vip.qq.com>
2021-06-03 23:46:32 +0800

    format codes.

--- models/NLResPRE.py
+++ models/NLResPRE.py
@@ -7,10 +7,11 @@ class ChannelAttention(nn.Module):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
-
-        self.fc = nn.Sequential(nn.Conv2d(in_planes, in_planes // 16, 1, bias=False),
-                                nn.ReLU(),
-                                nn.Conv2d(in_planes // 16, in_planes, 1, bias=False))
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_planes, in_planes // 16, 1, bias = False),
+            nn.ReLU(),
+            nn.Conv2d(in_planes // 16, in_planes, 1, bias = False)
+        )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -21,40 +22,35 @@ class ChannelAttention(nn.Module):
 
 
 class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
+    def __init__(self, kernel_size = 7):
         super(SpatialAttention, self).__init__()
-
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=False)
+        self.conv1 = nn.Conv2d(2, 1, kernel_size = kernel_size, padding = kernel_size // 2, bias = False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
+        avg_out = torch.mean(x, dim = 1, keepdim = True)
+        max_out, _ = torch.max(x, dim = 1, keepdim = True)
+        x = torch.cat([avg_out, max_out], dim = 1)
         x = self.conv1(x)
         return self.sigmoid(x)
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size = 3, stride = 1, padding = 1, bias = False)
         self.in1 = nn.InstanceNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.relu = nn.ReLU(inplace = True)
+
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size = 3, stride = 1, padding = 1, bias = False)
         self.in2 = nn.InstanceNorm2d(planes)
 
         self.ca = ChannelAttention(planes)
         self.sa = SpatialAttention()
 
-        self.downsample = downsample
-        self.stride = stride
-
     def forward(self, x):
-        residual = x
-
+        skip = x
+        
         out = self.conv1(x)
         out = self.in1(out)
         out = self.relu(out)
@@ -65,37 +61,29 @@ class BasicBlock(nn.Module):
         out = self.ca(out) * out
         out = self.sa(out) * out
 
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
+        out += skip
         out = self.relu(out)
-
         return out
 
 
 class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size = 1, stride = 1, bias = False)
         self.bn1 = nn.InstanceNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.InstanceNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self.bn3 = nn.InstanceNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
 
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size = 3, stride = 1, padding = 1, bias = False)
+        self.bn2 = nn.InstanceNorm2d(planes)
+
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size = 1, stride = 1, bias=False)
+        self.bn3 = nn.InstanceNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace = True)
+        
         self.ca = ChannelAttention(planes * 4)
         self.sa = SpatialAttention()
 
-        self.downsample = downsample
-        self.stride = stride
-
     def forward(self, x):
-        residual = x
+        skip = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -111,12 +99,8 @@ class Bottleneck(nn.Module):
         out = self.ca(out) * out
         out = self.sa(out) * out
 
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
+        out += skip
         out = self.relu(out)
-
         return out
 
 class NLResPRE(nn.Module):
@@ -124,7 +108,9 @@ class NLResPRE(nn.Module):
         super(NLResPRE, self).__init__()
         in_channel, out_channel = args.get('input_channel', 441), args.get('out_channel', 10)
         intermediate_channel = args.get('intermediate_channel', 64)
-        blocks, block_type = args.get('blocks', 22), args.get('block_type', 'BasicBlock')
+        blocks, block_type = int(args.get('blocks', 22)), args.get('block_type', 'BasicBlock')
+        if block_type not in ['BasicBlock', 'Bottleneck']:
+            raise AttributeError('Invalid block type, block type should be BasicBlock or Bottleneck')
         block = BasicBlock if block_type == 'BasicBlock' else Bottleneck
 
         self.conv1 = nn.Conv2d(in_channel, intermediate_channel, kernel_size=1, bias=False)
@@ -132,7 +118,7 @@ class NLResPRE(nn.Module):
         self.relu = nn.ReLU(inplace=True)
 
         layer = []
-        for i in range(0, int(blocks)):
+        for _ in range(blocks):
             layer.append(block(intermediate_channel, intermediate_channel))
         self.layer = nn.Sequential(*layer)
 
@@ -142,5 +128,4 @@ class NLResPRE(nn.Module):
         x = self.relu(self.in1(self.conv1(x)))
         x = self.layer(x)
         x = self.final(x)
-
         return x
--- models/ResPRE.py
+++ models/ResPRE.py
@@ -1,63 +1,58 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class net_pre(nn.Module):
-    def __init__(self, inplanes, planes, droprate):
-        super(net_pre, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, padding=1, bias=False)
+class BasicBlock(nn.Module):
+    def __init__(self, inplanes, planes, droprate = 0.2):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size = 3, padding = 1, bias = False)
         self.bn1 = nn.InstanceNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, padding=1, bias=False)
+        self.relu = nn.ReLU(inplace = True)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size = 3, padding = 1, bias = False)
         self.bn2 = nn.InstanceNorm2d(planes)
         self.droprate = droprate
 
     def forward(self, x):
-        residual = x
-
+        skip = x
         out = self.conv1(x)
         out = self.bn1(out)
         if self.droprate > 0:
-            out = F.dropout(out, p=self.droprate, training=self.training)
-
+            out = F.dropout(out, p = self.droprate, training = self.training)
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        if(self.droprate>0):
-            out = F.dropout(out, p=self.droprate, training=self.training)
-
-        out += residual
+        if self.droprate > 0:
+            out = F.dropout(out, p = self.droprate, training = self.training)
+        out += skip
         out = self.relu(out)
-
         return out
+
 
 class ResPRE(nn.Module):
     def __init__(self, args = {}):
+        super(ResPRE, self).__init__()
         inplanes = args.get('inplanes', 64)
         in_channel = args.get('input_channel', 441)
         out_channel = args.get('output_channel', 10)
         droprate = args.get('droprate', 0.2)
-        super(ResPRE, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, inplanes, kernel_size=1, bias=False)
+        blocks = int(args.get('blocks', 22))
+
+        self.conv1 = nn.Conv2d(in_channel, inplanes, kernel_size = 1, bias = False)
         self.bn1 = nn.InstanceNorm2d(inplanes)
         self.relu = nn.ReLU(inplace=True)
 
         layers = []
-        for i in range(0, 22):
-            layers.append(net_pre(inplanes, inplanes, droprate))
-
+        for _ in range(blocks):
+            layers.append(BasicBlock(inplanes, inplanes, droprate))
         self.layer = nn.Sequential(*layers)
-
-        self.lastlayer = nn.Conv2d(inplanes, out_channel, 3, padding=1, bias=False)
-
+        
+        self.lastlayer = nn.Conv2d(inplanes, out_channel, kernel_size = 3, padding = 1, bias = False)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-
         x = self.layer(x)
         x = self.lastlayer(x)
-
         return x
-
     
